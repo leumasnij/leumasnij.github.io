41dd653d14c1420954266768132d07b0d7544957
leumasnij <jinsamuel0911@gmail.com>
2026-09-23 21:52:49 -0400

    Update site

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -42,8 +42,8 @@ export const publications: Publication[] = [{
   image: "/images/kite.gif",
   imageAlt: "Cross-embodiment manipulation with KITE.",
   authors: [
-    { name: "Qianxu Wang", href: "https://qianxu.wang/", equalContribution: true },
-    { name: "Shengmiao Samuel Jin", isSelf: true, equalContribution: true },
+    { name: "Qianxu Wang", href: "https://qianxu.wang/" },
+    { name: "Shengmiao Samuel Jin", isSelf: true, },
     { name: "Kuan Fang", href: "https://kuanfang.github.io/",},
   ],
   links: [ ],
